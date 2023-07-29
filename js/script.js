4b0973026ddf30aab9f1c59fba0ed64170845ff1
Adriel Klem <adrielklem16@gmail.com>
2023-07-29 15:12:49 -0300

    Update js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
-// var Global
+// Var Global
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
 
-
+// Eventos
 document.querySelector("#actionEventStart").addEventListener("click",toggleScreen)
 document.querySelector("#actionEventReset").addEventListener("click",toggleScreen)
 
 
-
-// função callback
+// Função callback
 function toggleScreen() {
     screen1.classList.toggle('hide')
     screen2.classList.toggle('hide')
